3495e21f3067f83bfd28188a2622d5ed34bc6490
Héctor González <hectorgonzalezuno@gmail.com>
2020-10-21 18:11:10 -0300

    falta boton enviar

--- CinesArgentina/FrontEnd/src/app/components/users/users.component.ts
+++ CinesArgentina/FrontEnd/src/app/components/users/users.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
+  providers: [UserService]
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
--- CinesArgentina/FrontEnd/src/app/models/user.ts
+++ CinesArgentina/FrontEnd/src/app/models/user.ts
@@ -1,2 +1,12 @@
 export class User {
+
+    constructor(_id='', name='', password=''){
+        this._id=_id;
+        this.name=name;
+        this.password=password;
+    }
+
+    _id: string;
+    name: string;
+    password: string;
 }
